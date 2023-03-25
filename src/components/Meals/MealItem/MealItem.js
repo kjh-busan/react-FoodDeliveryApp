93e3f2760e587d40c76a7918f40a56e8c6d3d811
kjh-busan <picsmash7@gmail.com>
2023-03-26 00:22:12 +0900

    feat: MealItem

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -2,12 +2,16 @@ import React from "react";
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
   return (
-    <div className={classes.meal}>
-      <h3>{props.name}</h3>
-      <span>{props.description}</span>
-      <span>${props.price}</span>
-    </div>
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
   );
 };
 
