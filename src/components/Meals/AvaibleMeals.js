04cacbaedeabd1557e794279b46fe33012fdeece
kjh-busan <picsmash7@gmail.com>
2023-03-26 00:11:03 +0900

    feat: add MealItem

--- src/components/Meals/AvaibleMeals.js
+++ src/components/Meals/AvaibleMeals.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import { DUMMY_MEALS } from "./dummy-meals";
 import classes from "./AvaibleMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,14 @@
 import React from "react";
-import classes from "./MealItem.module.css ";
+import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
-  return <div className={classes.meal}>props.name</div>;
+  return (
+    <div className={classes.meal}>
+      <h3>{props.name}</h3>
+      <span>{props.description}</span>
+      <span>${props.price}</span>
+    </div>
+  );
 };
 
 export default MealItem;
