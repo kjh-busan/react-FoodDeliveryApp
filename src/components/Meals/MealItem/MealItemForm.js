49d1b0e11600897102329e1ff94fc5e4b6968d4c
kjh-busan <picsmash7@gmail.com>
2023-03-26 00:49:38 +0900

    fix: props id add

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,23 @@
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amout_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          defaultValue: "1",
+          step: "1",
+        }}
+      />
+      <button>+ ADD</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
